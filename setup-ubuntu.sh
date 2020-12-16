f0cd760aa728e274962e83f4bd0bee2747d856ec
David Villa Alises <david.villa@gmail.com>
2020-12-16 12:02:45 +0100

    vagrant scp format

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,3 +1,4 @@
 vagrant ssh ubuntu -- mkdir -p /home/vagrant/.ssh
 vagrant scp /usr/share/rubygems-integration/all/gems/vagrant-2.2.9/keys/vagrant.pub /home/vagrant/.ssh/authorized_keys ubuntu
+vagrant scp /usr/share/rubygems-integration/all/gems/vagrant-2.2.9/keys/vagrant.pub ubuntu:/home/vagrant/.ssh/authorized_keys
 vagrant ssh ubuntu -- chmod 0600 /home/vagrant/.ssh/authorized_keys
